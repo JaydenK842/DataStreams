a6702a00b9e23c92ede58d7087a49d456457ba93
JaydenK842 <knightjy@mail.uc.edu>
2023-04-05 10:58:12 -0400

    Initial Commit

---
+++ src/StreamGUI.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+
+public class StreamGUI extends JFrame {
+    JFrame frame;
+
+    JPanel middleSection;
+    JTextArea original;
+    JTextArea filtered;
+    JTextField searchField;
+
+    JPanel bottomSection;
+    JButton load;
+    JButton quit;
+    JButton compileSearch;
+
+    public StreamGUI() {
+        frame = new JFrame();
+        frame.setLayout(null);
+
+        /*
+        textLocation();
+        middleSection.setBounds();
+
+        buttons();
+        bottomSection.setBounds();
+         */
+    }
+
+    private void textLocation() {
+        middleSection = new JPanel();
+
+
+    }
+
+    private void buttons() {
+        bottomSection = new JPanel();
+
+
+    }
+
+    private void fileLoading() {
+
+    }
+
+    private void searchResults() {
+
+    }
+}
